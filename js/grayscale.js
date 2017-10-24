9e92f1d7b7f4f0bd1d10c8042a6be3dcbbbf8345
Tim Birkett <tim@birkett-bros.com>
2017-10-24 23:49:51 +0100

    Add map and FAQ

--- js/grayscale.js
+++ js/grayscale.js
@@ -34,3 +34,42 @@ $('.navbar-collapse ul li a').click(function() {
 $("a").mouseup(function(){
     $(this).blur();
 })
+
+// Google Maps Scripts
+var map = null;
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'resize', function() {
+  map.setCenter(new google.maps.LatLng(53.4722, -2.2412));
+});
+
+function init() {
+  // Basic options for a simple Google Map
+  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+  var mapOptions = {
+    // How zoomed in you want the map to start at (always required)
+    zoom: 16,
+
+    // The latitude and longitude to center the map (always required)
+    center: new google.maps.LatLng(53.4722, -2.2412), // New York
+
+    // Disables the default Google Maps UI components
+    disableDefaultUI: true,
+    scrollwheel: false,
+    draggable: false
+  }
+
+  // Get the HTML DOM element that will contain your map
+  // We are using a div with id="map" seen below in the <body>
+  var mapElement = document.getElementById('map');
+
+  // Create the Google Map using out element and options defined above
+  map = new google.maps.Map(mapElement, mapOptions);
+
+  // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
+  var myLatLng = new google.maps.LatLng(53.4722, -2.2412);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map
+  });
+}
