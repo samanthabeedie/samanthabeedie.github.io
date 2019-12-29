8f1743cca17a1f877331dbf6a379f19b492d963b
Tim Birkett <sharkfinhoop@gmail.com>
2019-12-29 22:30:16 +0000

    Fix map location

--- js/grayscale.js
+++ js/grayscale.js
@@ -48,13 +48,13 @@ function init() {
   // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
   var mapOptions = {
     // How zoomed in you want the map to start at (always required)
-    zoom: 18,
+    zoom: 16,
 
     // The latitude and longitude to center the map (always required)
     center: new google.maps.LatLng(53.3866533, -2.3490314), // New York
 
     // Disables the default Google Maps UI components
-    disableDefaultUI: false,
+    disableDefaultUI: true,
     scrollwheel: false,
     draggable: false
   }
